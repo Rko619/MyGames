6567931243bfd198f0b7170609a76e7327fbb669
Raghul <praghul1995@gmail.com>
2017-11-02 10:37:08 +0530

    mesh,scripts
    added mesh,scripts

---
+++ BarGraphVisualizer/Assets/_scripts/BarGraphManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarGraphManager : MonoBehaviour {
+	
+	public Transform origin;
+
+
+
+
+	struct BarData
+	{
+		string userName;
+		float age;
+		int maxAge;
+	};
+
+
+	void Start () 
+	{
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateBarGraph() {
+		
+		
+	}
+
+	void InitializeBarGraph() {
+		
+	}
+}
