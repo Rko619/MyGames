4aa293a5f17905c5152bbad02c8d1f118176d286
Raghul <praghul1995@gmail.com>
2017-11-02 12:31:04 +0530

    added - DrawBarGraph

--- BarGraphVisualizer/Assets/_scripts/BarGraphManager.cs
+++ BarGraphVisualizer/Assets/_scripts/BarGraphManager.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class BarGraphManager : MonoBehaviour {
 	
 	public Transform origin;
-
-
-
-
-	struct BarData
+	public GameObject cubePrefab;
+	public int maxAge;
+	public float spaceCorrectionValue;
+	[System.Serializable]
+	public struct BarData
 	{
-		string userName;
-		float age;
-		int maxAge;
+		public string userName;
+		public float age;
+		public GameObject cubePrefab;
 	};
+	public BarData[] userData;
+
+
+	private float spaceBetweenBars;
+
+
 
 
 	void Start () 
 	{
-
-
-		
+		spaceBetweenBars = cubePrefab.GetComponent<Renderer> ().bounds.extents.z+spaceCorrectionValue;
+		InitializeBarGraph ();	
 	}
 	
 	// Update is called once per frame
@@ -29,12 +34,22 @@ public class BarGraphManager : MonoBehaviour {
 		
 	}
 
-	void UpdateBarGraph() {
+	void DrawBarGraph() {
 		
-		
+		for (int i = 0; i < userData.Length; i++) {
+			userData [i].cubePrefab.transform.localScale = new Vector3 (userData [i].cubePrefab.transform.localScale.x, userData [i].age, userData [i].cubePrefab.transform.localScale.y);
+		}
 	}
 
 	void InitializeBarGraph() {
-		
+		Vector3 lastPosition=Vector3.zero;
+		float cubeBoundZValue = 0;
+		cubeBoundZValue=cubePrefab.GetComponent<Renderer> ().bounds.extents.z*2f;
+		for (int i = 0; i < userData.Length; i++) {
+			lastPosition = new Vector3(origin.position.x,origin.position.y,cubeBoundZValue+lastPosition.z+ spaceBetweenBars);
+			userData[i].cubePrefab=Instantiate (cubePrefab, lastPosition, Quaternion.identity);
+		}
+
+		DrawBarGraph ();
 	}
 }
---
+++ BarGraphVisualizer/Assets/_scripts/vfsdgs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FileManager : MonoBehaviour {
+
+	// Use this for initialization
+	private string filePath;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenFile()
+	{
+		filePath = EditorUtility.OpenFilePanel ("Open the excel sheet", "D", "csv");
+		Debug.Log ("File Location="+filePath);
+	}
+}
